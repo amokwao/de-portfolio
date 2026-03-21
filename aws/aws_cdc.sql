8b3340efdfb900d2cfcbe20887728ca1debeb140
Godwin Amo-Kwao <amokwao@gmail.com>
2026-03-21 13:24:41 -0700

    Keep main branch minimal with README only

--- aws/aws_cdc.sql
+++
@@ -1,8 +0,0 @@
--- Insert
-INSERT INTO retail.orders (customer_id, status, total) VALUES (1, 'NEW', 300);
-
--- Update
-UPDATE retail.orders SET status='SHIPPED' WHERE order_id=1;
-
--- Delete
-DELETE FROM retail.orders WHERE order_id=2;
